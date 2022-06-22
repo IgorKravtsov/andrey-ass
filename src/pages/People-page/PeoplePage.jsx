5609c6d7d3c789d6e157357295668204daf902cf
Reinmir <studenko.andrey@gmail.com>
2022-06-22 12:13:26 +0300

    Abrakadabra

--- src/pages/People-page/PeoplePage.jsx
+++ src/pages/People-page/PeoplePage.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState }  from 'react'
+import styles from  './style.module.scss'
 import { Link } from 'react-router-dom'
+import axios from 'axios';
 
 function PeoplePage (){
+  const [appState, setAppState] = useState("");
+    useEffect(()=>{
+        const apiUrl = 'https://swapi.dev/api/people/1/';
+        axios.get(apiUrl).then((resp) =>{
+            const allPersons = resp.data;
+            setAppState(allPersons);
+        });
+    }, [setAppState]);
+    useEffect(() =>{
+        console.log(appState);
+    }, [appState]);
     return(
         <>
-        <h1>Игорь сказал это надо сделать</h1>
+        <Link className={styles.toHome} to={"/"}>Home</Link>
         </>
+        
     )
+    
 }
 
 export default PeoplePage
